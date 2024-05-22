d43e7213c75f05499b81d495e3771297de7d5021
ramos013 <334020@student.mboutrecht.nl>
2024-05-22 11:22:14 +0200

    start up

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+    <x-slot:heading>
+        Contact Page
+    </x-slot:heading>
+
+    <h1>hello world thew contact page</h1>
+
+</x-layout>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <x-slot:heading>
+        Home Page
+    </x-slot:heading>
+    <h1>hello world thew home page</h1>
+</x-layout>
