e15735ec3d08eb4d0c9c13253d98726df98a57ed
ramos013 <334020@student.mboutrecht.nl>
2024-05-29 13:45:39 +0200

    two key eloquent relationship types

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -16,4 +16,9 @@ class Job extends Model
 
     protected $table = 'job_listings';
     protected $fillable = ['title', 'description', 'salary'];
+
+    public function employer()
+    {
+        return $this->belongsTo(Employer::class);
+    }
 }
